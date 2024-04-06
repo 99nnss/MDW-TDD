84f94043ca459fd8bbdc0861b0e7b3592419887d
LeLiu <le.liu@alumnos.upm.es>
2024-04-06 19:38:12 +0200

    Red

--- TDD/src/test/IntervalTest.java
+++ TDD/src/test/IntervalTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import main.Interval;
@@ -10,6 +11,11 @@ public class IntervalTest {
     @Test
     public void givenIntervalWhenIncludeWithIncludedValueThenTrue(){
         assertTrue(new Interval(-1.7,5555.0).include(0.0));
+    }
+
+    @Test
+    public void givenIntervalWhenIncludeWithNotIncludedValueThenFalse(){
+        assertFalse(new Interval(-1.7,5555.0).include(9000.0));
     }
 
 }
