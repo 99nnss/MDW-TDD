bf9f424c235511381ac0fb241d43e6f388181e9d
LeLiu <le.liu@alumnos.upm.es>
2024-04-06 20:05:49 +0200

    Green

--- TDD/src/main/Interval.java
+++ TDD/src/main/Interval.java
@@ -3,14 +3,19 @@ package main;
 public class Interval {
 
     private double min,max;
+    private boolean maxOpen;
 
     public Interval(boolean minOpen, double min, boolean maxOpen, double max) {
         assert min <= max;
+        this.maxOpen = maxOpen;
         this.max = max;
         this.min = min;
     }
 
     public boolean include(double value) {
+        if (this.maxOpen){
+            return this.min <= value && value < this.max;
+        }
         return this.min <= value && value <= this.max;
     }
 
