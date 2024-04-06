c914461e4394dba92fde39aea2460da5ea63fa81
LeLiu <le.liu@alumnos.upm.es>
2024-04-06 22:00:27 +0200

    Add IncludedMax test: Green

--- TDD/src/test/IncludedMaxTest.java
+++ TDD/src/test/IncludedMaxTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import main.IncludedMax;
@@ -7,9 +8,20 @@ import org.junit.jupiter.api.Test;
 
 public class IncludedMaxTest {
 
+
     @Test
     public void givenMaxClosedWhenGreaterWithLessValueThenTrue(){
         assertTrue(new IncludedMax(4).isOnLeft(0.0));
+    }
+
+    @Test
+    public void givenMaxClosedWhenGreaterWithGreaterValueThenFalse(){
+        assertFalse(new IncludedMax(4).isOnLeft(5.0));
+    }
+
+    @Test
+    public void givenMaxCloseWhenGreaterWithEqualValueThenFalse(){
+        assertTrue(new IncludedMax(4).isOnLeft(4));
     }
 
 }
